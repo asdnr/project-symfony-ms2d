ab3efa9d66c2ccaf20706a13a99e808aef60fb5c
Asmaa <asmaa.dinar@oui-connect.fr>
2022-12-06 16:11:39 +0100

    1er commit installation

---
+++ src/Controller/UserController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;//Import annotation "Request"
+use Doctrine\Persistence\ManagerRegistry;
+
+class UserController extends AbstractController
+{
+  #[Route('/user', name: 'app_user')]
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/UserController.php',
+        ]);
+    }
+
+
+    #[Route('/user/listes_users', name: 'app_user_listesusers', methods: 'GET')]   
+    public function listesUsers(ManagerRegistry $doctrine)
+    {
+        $entityManager = $doctrine->getManager();
+
+        $repository = $entityManager->getRepository(User::class); // requete pour trouve le user via l'id mis dans l'url
+        $users= $repository->findAll();
+      
+       
+       
+      return $this->render('./users/listes_users.html.twig', [ 'users' => $users ]);
+    }
+
+
+
+
+}
